d42ae39786a369e987b4f87a1b0855288a874596
practicum <practicum@mac.local>
2023-06-21 09:00:54 +0400

    fix comma

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ first_line=$(echo "$remote" | sed -n '1p')
 
 # Check if the first line is empty or contains "github.com:yandex-praktikum"
 if [ -z "$first_line" ] || [[ "$first_line" == *"github.com:yandex-praktikum"* ]]; then
-    echo "Упс. Кажется вы не сделали Fork репозитория!"
+    echo "Упс. Кажется, вы не сделали Fork репозитория!"
 else
     echo "Секретный ключ: practicum"
 fi
